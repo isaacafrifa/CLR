0f54342d6e6917832d2aab500c6178e4603bd7fa
Yaw Afrifa <isaac.afrifa3@yahoo.com>
2021-02-28 01:38:44 +0000

    increased token expiry time

--- src/main/java/com/blo/model/Constants.java
+++ src/main/java/com/blo/model/Constants.java
@@ -5,7 +5,9 @@ public final class Constants {
 
 	// constant ALLOWED_ORIGIN is public because we anticipate needing to access it
 	// outside of our package
-	public static final String ALLOWED_ORIGIN = "https://myclr.netlify.app"; 
+	public static final String ALLOWED_ORIGIN = "https://myclr.netlify.app";  //
+
+	public static final long EXPIRE_TOKEN_AFTER_MINUTES = 30;
 
 	// we've defined a private constructor so it can't be instantiated
 	private Constants() {
